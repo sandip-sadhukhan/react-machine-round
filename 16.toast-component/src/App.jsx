02dff48d9f948e899b6a1b462944ddf0c564ca99
Sandip Sadhukhan <sandippakhanna@gmail.com>
2024-09-21 07:46:12 +0530

    toast component part 1

---
+++ 16.toast-component/src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+import Notification from './components/notification'
+import useNotification from './hooks/use-notification'
+
+function App() {
+  const {NotificationComponent, triggerNotification} = useNotification("bottom-right")
+
+  return (
+    <>
+    <h1>Notification component</h1>
+    <button
+      onClick={() => triggerNotification({
+        type: "success",
+        message: "File Sent Successfully",
+        duration: 3000
+      })}
+    >
+      Trigger success
+    </button>
+
+    <button
+      onClick={() => triggerNotification({
+        type: "error",
+        message: "File Sent Failed",
+        duration: 3000
+      })}
+    >
+      Trigger failed
+    </button>
+
+    {NotificationComponent}
+    </>
+  )
+}
+
+export default App
